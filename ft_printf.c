4c39036ae4716f7ad1813537360c1bc2d5eaa826
Kaan Gucluer <kgucluer@k2m13s04.42kocaeli.com.tr>
2023-07-13 17:28:01 +0300

    vol2

--- ft_printf.c
+++ ft_printf.c
@@ -6,41 +6,47 @@
 /*   By: kgucluer <kgucluer@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 13:04:39 by kgucluer          #+#    #+#             */
-/*   Updated: 2023/07/13 16:40:43 by kgucluer         ###   ########.fr       */
+/*   Updated: 2023/07/13 17:27:47 by kgucluer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
+
 // malloc, free, write, va_start, va_arg, va_copy,
-//va_end HARİCİ FONKSİYONLAR
-int ft_putchar(char k)
+// va_end HARİCİ FONKSİYONLAR
+int	ft_putchar(char k)
 {
-    write(1, &k, 1);
+	return (write(1, &k, 1));
 }
 
-int type_convert(va_list va, char type)
+int	type_convert(va_list va, char type)
 {
-
 }
 
-
-int ft_printf(const char *str, ...)
+int	ft_printf(const char *str, ...)
 {
-    va_list args;
-    int index;
-    int total_byte;
+	va_list args;
+	int index;
+	int total_byte;
+	int tmp;
 
-    va_start(str,args);
-    index = 0;
-    total_byte = 0;
-    while(str[i])
-    {
-        if(str[i] == '%')
-            total_byte += type_convert(args,str[++i]);
-        else if (write(1,&str[i],1) == -1)
-            return -1;
-        i++;
-    }
-    va_end(args);
-    return(total_byte);
+	va_start(str, args);
+	index = 0;
+	total_byte = 0;
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			tmp = type_convert(args, str[++i]);
+			if (tmp == -1)
+				return (-1);
+			total_byte += tmp - 1;
+		}
+		else if (write(1, &str[i], 1) == -1)
+			return (-1);
+		total_byte++;
+		i++;
+	}
+	va_end(args);
+	return (total_byte);
 }
